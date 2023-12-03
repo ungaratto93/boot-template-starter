25bf63cd28c6c36618f60bd18e963dae877ed3b4
ungaratto93 <eduardo.ungarattolima@gmail.com>
2023-12-02 23:16:32 -0300

    upload template

---
+++ src/test/java/br/com/ungaratto93/demolayered/DemoLayeredApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ungaratto93.demolayered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLayeredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
