e7db1414289ee12d80fa60392bbbf8a7d20d9255
ungaratto93 <eduardo.ungarattolima@gmail.com>
2023-12-09 01:05:28 -0300

    Add active profile test in LayersApplicationTests.java file

--- src/test/java/br/com/ungaratto93/layers/LayersApplicationTests.java
+++ src/test/java/br/com/ungaratto93/layers/LayersApplicationTests.java
@@ -2,6 +2,8 @@ package br.com.ungaratto93.layers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @ActiveProfiles("test")
